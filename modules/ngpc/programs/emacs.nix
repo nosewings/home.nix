e87c8af05db02f213411ea63baa3660eed6e3017
Nicholas Coltharp <coltharp@pdx.edu>
2021-11-27 08:49:33 -0600

    Tweaked org-mode keybinds

--- modules/ngpc/programs/emacs.nix
+++ modules/ngpc/programs/emacs.nix
@@ -111,10 +111,11 @@ in
             bind = {
               "" = {
                 "\"C-c o r a a\"" = "org-roam-alias-add";
-                "\"C-c o r f\"" = "org-roam-node-find";
-                "\"C-c o r i\"" = "org-roam-node-insert";
-                "\"C-c o r s\"" = "org-roam-db-sync";
-                "\"C-c o r t\"" = "org-roam-buffer-toggle";
+                "\"C-c o r b t\"" = "org-roam-buffer-toggle";
+                "\"C-c o r d s\"" = "org-roam-db-sync";
+                "\"C-c o r n f\"" = "org-roam-node-find";
+                "\"C-c o r n i\"" = "org-roam-node-insert";
+                "\"C-c o r t a\"" = "org-roam-tag-add";
               };
             };
             custom = {
