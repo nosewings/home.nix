49c422d0b29d9e9d104459fac52f6f07b185e590
Nicholas Coltharp <coltharp@pdx.edu>
2021-12-11 01:27:08 -0600

    Fonts: update

--- modules/ngpc/fonts.nix
+++ modules/ngpc/fonts.nix
@@ -7,15 +7,21 @@ in
   options.ngpc.fonts = {
     enable = mkEnableOption "Fonts config";
   };
-
-  config = mkIf cfg.enable {
-    fonts.fontconfig.enable = true;
-    home.packages = with pkgs; [
-      dejavu_fonts
-      iosevka
-      overpass
-      roboto
-      ubuntu_font_family
-    ];
-  };
+  config = mkIf cfg.enable (mkMerge [
+    {
+      fonts.fontconfig.enable = true;
+      home.packages = with pkgs; [
+        dejavu_fonts
+        iosevka
+        nerdfonts
+        overpass
+        roboto
+        ubuntu_font_family
+      ];
+    }
+    {
+      allowUnfreePredicate = pkg: elem (getName pkg) [ "symbola" ];
+      home.packages = with pkgs; [ symbola ];
+    }
+  ]);
 }
