55bf25b6dd924bc94ce7b0ab40a1ab2dd18c6407
Nicholas Coltharp <coltharp@pdx.edu>
2021-12-10 22:28:36 -0600

    Add SSH config

--- home.nix
+++ home.nix
@@ -82,6 +82,7 @@ with lib;
 
   ngpc.programs.direnv.enable = true;
   ngpc.programs.emacs.enable = true;
+  ngpc.programs.ssh.enable = true;
 
   ngpc.languages.nix = {
     enable = true;
---
+++ modules/ngpc/programs/ssh.nix
@@ -0,0 +1,20 @@
+with builtins;
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.ngpc.programs.ssh;
+in
+{
+  options.ngpc.programs.ssh = {
+    enable = mkEnableOption "SSH config";
+  };
+
+  config = mkIf cfg.enable {
+    programs.ssh.enable = true;
+    home.activation = {
+      "ssh.authorized_keys" = hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD eval "curl $VERBOSE_ARG 'https://github.com/nosewings.keys' > '${config.home.homeDirectory}/.ssh/authorized_keys'"
+      '';
+    };
+  };
+}
