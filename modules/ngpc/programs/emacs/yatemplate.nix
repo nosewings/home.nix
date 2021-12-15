e5a306d02a6bf2c3ecfe572f7e9a42e677898335
Nicholas Coltharp <coltharp@pdx.edu>
2021-12-15 14:34:39 -0600

    Emacs config: add YATemplate

---
+++ modules/ngpc/programs/emacs/yatemplate.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+with lib;
+let
+  cfg = config.ngpc.programs.emacs.yatemplate;
+in
+{
+  options.ngpc.programs.emacs.yatemplate = {
+    enable = mkEnableOption "Emacs YATemplate config";
+    templateFiles = mkOption {
+      type = with types; attrsOf path;
+      default = {};
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.emacs.init.init.packages.yatemplate = {
+      enable = true;
+      config = ''
+        (yas-global-mode)
+        (yatemplate-fill-alist)
+      '';
+    };
+    home.file = flip mapAttrs' cfg.templateFiles (file: source:
+       nameValuePair ".config/emacs/templates/${file}" { inherit source; }
+    );
+  };
+}
