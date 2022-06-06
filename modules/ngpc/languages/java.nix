3ab06ec461d81671246fd55c7548f4b1160efa22
Nicholas Coltharp <coltharp@pdx.edu>
2022-06-06 00:46:23 -0500

    emacs: make the .java template guess the package

--- modules/ngpc/languages/java.nix
+++ modules/ngpc/languages/java.nix
@@ -17,6 +17,34 @@ in
       "*.jar"
     ];
     programs.emacs.init.init.packages = {
+      cc-mode = {
+        enable = true;
+        package = null;
+        preface = ''
+          (defun ngpc/maven-root (file)
+            (f-full (locate-dominating-file file "pom.xml")))
+          (defun ngpc/maven-source-root (file)
+            (-if-let (mvn-root (f-full (ngpc/maven-root file)))
+              (->> '("src/main/java"
+                     "src/main/resources"
+                     "src/main/filters"
+                     "src/main/webapp"
+                     "src/test/java"
+                     "src/test/resources"
+                     "src/test/filters"
+                     "src/it"
+                     "src/assembly"
+                     "src/site"
+                     "")
+                   (--map (f-join mvn-root it))
+                   (--first (s-starts-with? it file)))))
+          (defun ngpc/maven-guess-package (file)
+            (-some->> (ngpc/maven-source-root file)
+                      (f-relative file)
+                      (f-parent)
+                      (directory-file-name)
+                      (s-replace "/" ".")))'';
+      };
       lsp-java = {
         enable = true;
         hook = {
--- modules/ngpc/languages/templates/template.java
+++ modules/ngpc/languages/templates/template.java
@@ -1,3 +1,5 @@
+package ${1:`(or (ngpc/maven-guess-package (buffer-file-name)) "mypackage")`};
+
 class `(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))` {
     $0
 }
