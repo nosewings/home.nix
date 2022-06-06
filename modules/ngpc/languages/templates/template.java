5c68faf81a2df24dd7c1a42028e59e2a294f0d34
Nicholas Coltharp <coltharp@pdx.edu>
2022-06-06 15:59:02 +0000

    emacs: touch-up java template

--- modules/ngpc/languages/templates/template.java
+++ modules/ngpc/languages/templates/template.java
@@ -1,4 +1,4 @@
-package ${1:`(or (ngpc/maven-guess-package (buffer-file-name)) "mypackage")`};
+package ${1:`(or (ngpc/maven-guess-package (buffer-file-name)) "com.example.mypackage")`};
 
 class `(file-name-nondirectory (file-name-sans-extension (buffer-file-name)))` {
     $0
