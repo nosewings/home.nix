cd0f8cfa40e8c5b8424325cc5ec879f1fde0f851
Nicholas Coltharp <coltharp@pdx.edu>
2022-06-09 19:02:46 +0000

    emacs: set vterm buffer name by perspective
    This makes it so that typing `M-x vterm` will take us to a vterm session
    in our own workspace.

--- modules/ngpc/programs/emacs/vterm.nix
+++ modules/ngpc/programs/emacs/vterm.nix
@@ -8,20 +8,41 @@ in
     enable = mkEnableOption "Emacs vterm config";
   };
 
-  config = mkIf cfg.enable {
-    programs.emacs.vterm.enable = true;
-    programs.emacs.init.init.packages = {
-      vterm = {
-        enable = true;
-        hook = {
-          vterm-mode = [
-            "ngpc/disable-hl-line"
-          ];
+  config = mkIf cfg.enable (mkMerge [
+    {
+      programs.emacs.vterm.enable = true;
+      programs.emacs.init.init.packages = {
+        vterm = {
+          enable = true;
+          hook = {
+            vterm-mode = [
+              "ngpc/disable-hl-line"
+            ];
+          };
+        };
+        vterm-toggle = {
+          enable = true;
         };
       };
-      vterm-toggle = {
-        enable = true;
+    }
+    (mkIf (config.ngpc.programs.emacs.perspectives == "perspective") {
+      programs.emacs.init.init.packages.vterm = {
+        config = ''
+          (when persp-mode
+            (ngpc/persp-set-vterm-buffer-name))'';
       };
-    };
-  };
+      programs.emacs.init.init.packages.perspective = {
+        preface = ''
+          (defun ngpc/persp-set-vterm-buffer-name ()
+            (setq vterm-buffer-name (concat "*vterm "
+                                            (car persp-modestring-dividers)
+                                            (persp-name (persp-curr))
+                                            (cadr persp-modestring-dividers)
+                                            "*")))'';
+        hook = {
+          persp-activated = [ "ngpc/persp-set-vterm-buffer-name" ];
+        };
+      };
+    })
+  ]);
 }
