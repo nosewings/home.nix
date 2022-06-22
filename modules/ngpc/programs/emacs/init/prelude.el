d6833bfabc29d912929faa7b6a9cc3cdd94e4202
Nicholas Coltharp <coltharp@pdx.edu>
2022-06-22 21:00:30 +0000

    Better binding for backward-kill-sexp

--- modules/ngpc/programs/emacs/init/prelude.el
+++ modules/ngpc/programs/emacs/init/prelude.el
@@ -90,6 +90,7 @@
 
 (global-set-key (kbd "C-;") #'previous-buffer)
 (global-set-key (kbd "C-'") #'next-buffer)
+(global-set-key (kbd "C-M-<backspace>") #'backward-kill-sexp)
 
 (defun ngpc/switch-to-prev-buffer-skip-function (var)
   "Convert VAR into a function for `switch-to-prev-buffer-skip'."
