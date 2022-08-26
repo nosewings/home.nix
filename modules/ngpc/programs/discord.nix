19ef59dfbe086ff2ece944aee88936a082b607d6
Nicholas Coltharp <coltharp@pdx.edu>
2022-08-26 03:24:03 -0500

    Make Discord start even when an update is ready

--- modules/ngpc/programs/discord.nix
+++ modules/ngpc/programs/discord.nix
@@ -11,5 +11,11 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ discord ];
     allowUnfreePredicate = pkg: elem (getName pkg) [ "discord" ];
+    # Stop Discord from refusing to start when an update is available.
+    xdg.configFile."discord/settings.json".text = ''
+      {
+        "SKIP_HOST_UPDATE": true
+      }
+    '';
   };
 }
